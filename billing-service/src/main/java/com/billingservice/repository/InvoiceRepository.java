aeeffcc934f2d875f0e079960249dd976ae06882
Sharanya Maddha <sharanyamaddha@gmail.com>
2025-12-31 15:49:04 +0530

    added apis related to invoice

---
+++ billing-service/src/main/java/com/billingservice/repository/InvoiceRepository.java
@@ -0,0 +1,15 @@
+package com.billingservice.repository;
+
+import java.util.List;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import com.billingservice.model.Invoice;
+
+@Repository
+public interface InvoiceRepository extends MongoRepository<Invoice, String>{
+
+	List<Invoice> findByCustomerId(String customerId);
+
+}
