ac05f4f0a01d6246f637c41211cccaa0332ce4b4
Sharanya Maddha <sharanyamaddha@gmail.com>
2026-01-01 20:54:38 +0530

    added feign config

---
+++ billing-service/src/main/java/com/billingservice/config/FeignHttpConfig.java
@@ -0,0 +1,16 @@
+package com.billingservice.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import feign.Client;
+import feign.httpclient.ApacheHttpClient;
+
+@Configuration
+public class FeignHttpConfig {
+
+    @Bean
+    public Client feignClient() {
+        return new ApacheHttpClient();
+    }
+}
