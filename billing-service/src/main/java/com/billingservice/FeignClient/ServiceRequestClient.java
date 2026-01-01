fce3d6129f27a9a1eeff30bb0ec8e53abfd1ee02
Sharanya Maddha <sharanyamaddha@gmail.com>
2026-01-01 20:56:00 +0530

    updated service request client

--- billing-service/src/main/java/com/billingservice/FeignClient/ServiceRequestClient.java
+++ billing-service/src/main/java/com/billingservice/FeignClient/ServiceRequestClient.java
@@ -3,10 +3,12 @@ package com.billingservice.FeignClient;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestHeader;
 
+import com.billingservice.config.FeignConfig;
 import com.billingservice.dto.ServiceRequestDTO;
 
-@FeignClient(name = "service-request-service")
+@FeignClient(name = "service-request-service", configuration = FeignConfig.class)
 public interface ServiceRequestClient {
 
     @GetMapping("/api/service-requests/{id}")
