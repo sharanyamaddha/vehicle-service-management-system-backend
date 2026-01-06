92ce2ce7b120c1052baa1125cfe4b2a6b441c744
Sharanya Maddha <sharanyamaddha@gmail.com>
2026-01-06 23:32:11 +0530

    updated files

--- api-gateway/src/main/java/com/apigateway/security/JwtAuthInterceptor.java
+++ api-gateway/src/main/java/com/apigateway/security/JwtAuthInterceptor.java
@@ -12,16 +12,20 @@ import org.springframework.web.servlet.HandlerInterceptor;
 @Component
 public class JwtAuthInterceptor implements HandlerInterceptor {
 
-    private static final String SECRET =
-        "THIS_IS_VMS_SUPER_SECURE_32BYTE_LONG_SECRET_KEY_2025";
+    private static final String SECRET = "THIS_IS_VMS_SUPER_SECURE_32BYTE_LONG_SECRET_KEY_2025";
 
     @Override
     public boolean preHandle(HttpServletRequest request,
-                             HttpServletResponse response,
-                             Object handler) throws Exception {
-    	String internalCall = request.getHeader("X-Internal-Call");
-    	if ("true".equals(internalCall)) return true;
+            HttpServletResponse response,
+            Object handler) throws Exception {
 
+        if ("OPTIONS".equalsIgnoreCase(request.getMethod())) {
+            response.setStatus(HttpServletResponse.SC_OK);
+            return true;
+        }
+        String internalCall = request.getHeader("X-Internal-Call");
+        if ("true".equals(internalCall))
+            return true;
 
         if (request.getDispatcherType() != DispatcherType.REQUEST)
             return true;
@@ -48,48 +52,56 @@ public class JwtAuthInterceptor implements HandlerInterceptor {
 
             String role = claims.get("role", String.class);
 
-         // ADMIN – full access
-         if ("ADMIN".equals(role)) return true;
+            // ADMIN
+            if ("ADMIN".equals(role))
+                return true;
 
-         // MANAGER permissions
-         if ("MANAGER".equals(role)) {
-             if (
-                 path.contains("/assign") ||
-                 path.contains("/parts/approve") ||
-                 path.contains("/close") ||
-                 path.startsWith("/api/service-requests/manager") ||
-                 path.startsWith("/api/technicians") ||
-                 path.startsWith("/api/parts") ||
-                 path.startsWith("/api/bays") ||
-                 path.startsWith("/api/invoices")
-             ) return true;
-         }
+            // MANAGER
+            if ("MANAGER".equals(role)) {
+                if (path.startsWith("/api/users/me") ||
+                        path.startsWith("/api/users") ||
+                        path.contains("/assign") ||
+                        path.contains("/parts/approve") ||
+                        path.contains("/close") ||
+                        path.startsWith("/api/service-requests/") ||
+                        path.startsWith("/api/service-requests/manager") ||
+                        path.startsWith("/api/technicians") ||
+                        path.startsWith("/api/parts") ||
+                        path.startsWith("/api/bays") ||
+                        path.startsWith("/api/vehicles") ||
+                        path.startsWith("/api/invoices"))
+                    return true;
+            }
 
-         // TECHNICIAN permissions
-         if ("TECHNICIAN".equals(role)) {
-             if (
-                 path.contains("/start") ||
-                 path.contains("/status") ||
-                 path.contains("/parts/request") ||
-                 path.startsWith("/api/service-requests/technician")
-             ) return true;
-         }
+            // TECHNICIAN
+            if ("TECHNICIAN".equals(role)) {
+                if (path.startsWith("/api/users/me") ||
+                        path.contains("/start") ||
+                        path.contains("/status") ||
+                        path.contains("/parts/request") ||
+                        path.startsWith("/api/parts") ||
+                        path.startsWith("/api/vehicles") ||
+                        path.startsWith("/api/service-requests/technician"))
+                    return true;
+            }
 
-         // CUSTOMER permissions
-         if ("CUSTOMER".equals(role)) {
-             if (
-                 path.equals("/api/service-requests") ||
-                 path.startsWith("/api/service-requests/customer") ||
-                 path.startsWith("/api/vehicles") ||
-                 path.startsWith("/api/invoices/customer") ||
-                 path.startsWith("/api/users/me") ||
-                 path.contains("/api/invoices") && path.contains("/pay")
+            // CUSTOMER
+            if ("CUSTOMER".equals(role)) {
+                if (path.equals("/api/service-requests") ||
+                        path.startsWith("/api/service-requests/customer") ||
+                        path.startsWith("/api/vehicles") ||
+                        path.startsWith("/api/invoices/customer") ||
+                        path.startsWith("/api/users/me") ||
+                        (path.contains("/api/invoices") && path.contains("/pay")) ||
+                        path.startsWith("/api/payments") ||
+                        (request.getMethod().equals("GET") && path.matches("/api/service-requests/[^/]+"))
 
-             ) return true;
-         }
+                )
+                    return true;
+            }
 
-         response.setStatus(403);
-         return false;
+            response.setStatus(403);
+            return false;
 
         } catch (Exception e) {
             response.setStatus(401);
