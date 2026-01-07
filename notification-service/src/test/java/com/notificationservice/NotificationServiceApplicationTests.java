a16baeb5cb4f550502476cc421848b939d1b9c01
Sharanya Maddha <sharanyamaddha@gmail.com>
2026-01-07 07:37:23 +0530

    updated

--- notification-service/src/test/java/com/notificationservice/NotificationServiceApplicationTests.java
+++ notification-service/src/test/java/com/notificationservice/NotificationServiceApplicationTests.java
@@ -4,6 +4,10 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
+@org.springframework.test.context.TestPropertySource(properties = {
+		"MAIL_USERNAME=test",
+		"MAIL_PASSWORD=test"
+})
 class NotificationServiceApplicationTests {
 
 	@Test
