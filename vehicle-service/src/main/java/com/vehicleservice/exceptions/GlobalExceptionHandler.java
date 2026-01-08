1c9906dd67d1e473db9a0ec8cc02bc91f4d227d9
Sharanya Maddha <sharanyamaddha@gmail.com>
2026-01-08 22:14:16 +0530

    Fixed notification service tests and vehicle service error

---
+++ vehicle-service/src/main/java/com/vehicleservice/exceptions/GlobalExceptionHandler.java
@@ -0,0 +1,32 @@
+package com.vehicleservice.exceptions;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(RuntimeException.class)
+    public ResponseEntity<Map<String, String>> handleRuntimeException(RuntimeException ex) {
+        ex.printStackTrace(); // Print to logs to be sure
+        Map<String, String> error = new HashMap<>();
+        error.put("error", "Runtime Error");
+        error.put("message", ex.getMessage());
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(error);
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<Map<String, String>> handleGenericException(Exception ex) {
+        ex.printStackTrace(); // Print to logs
+        Map<String, String> error = new HashMap<>();
+        error.put("error", "Internal Server Error");
+        error.put("message", ex.getMessage());
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(error);
+    }
+
+}
